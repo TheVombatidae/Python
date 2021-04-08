da997ae86b7125ae37fbf72291362383c8e3424c
Marco Poggi <71911407+TheVombatidae@users.noreply.github.com>
2021-04-07 23:37:20 -0300

    Delete Grupo/Actividad_1 directory

--- Grupo/Actividad_1/Listas.py
+++
@@ -1,5 +0,0 @@
-import Cads
-#listas 
-nombre = Cads.nombres.replace('"','').replace(',','').replace(' ','').split('\n')
-eva1 = Cads.evas1.replace(',','').replace(' ','').split('\n')
-eva2 = Cads.evas2.replace(',','').replace(' ','').split('\n')  
